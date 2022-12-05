2c8048767ee030b7603c5bb0faf41bc85437ccc1
Akhmed <ahmedchahkiev001@gmail.com>
2022-12-05 10:41:14 +0300

    Сделал главный экран

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,6 @@
-import Footer from "../pages/Footer";
-import Header from "../pages/Header";
-import MainPage from "../pages/Main/MainPage";
+import Header from "./Header";
+import Footer from "./Footer";
+import MainPage from "./Main/MainPage";
 
 const App = () => {
     return ( 
