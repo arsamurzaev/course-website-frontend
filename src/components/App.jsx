5f9997d1d916dbb63766431b61c89ef48184b159
ridvan-yandaev <jorikridvan@mail.ru>
2022-12-06 16:17:41 +0300

    сделал регистрация компонент папку RegProfile

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,6 +3,7 @@ import Header from "./Header";
 import Footer from "./Footer";
 import MainPage from "./Main/MainPage";
 import RegisterPage from "../pages/RegisterPage/RegisterPage";
+import RegProfile from "../pages/RegProfile/RegProfile";
 
 // import CurrentCoursePage from "../pages/currentCoursePage/CurrentCoursePage";
 
@@ -12,6 +13,7 @@ const App = () => {
     <>
       <Header />
       <RegisterPage />
+      <RegProfile />
       <MainPage />
       <Footer />
       
