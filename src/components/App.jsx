fd0f8099c26b41675f1e5108cfe3e0b604c2a7d6
ridvan-yandaev <jorikridvan@mail.ru>
2022-12-06 15:20:27 +0300

    сделал компонент регистрация папки RegisterPage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,16 @@
 import Header from "./Header";
 import Footer from "./Footer";
 import MainPage from "./Main/MainPage";
+import RegisterPage from "../pages/RegisterPage/RegisterPage";
 
 const App = () => {
   return (
     <>
       <Header />
+      <RegisterPage />
       <MainPage />
       <Footer />
+      
     </>
   );
 };
