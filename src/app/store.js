c91b01689beec0d8221ac77cd989f8eeb816cf78
Alialisk <99403653+Aliasik@users.noreply.github.com>
2022-12-08 16:23:11 +0300

    Вывод курсов закончен

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import application from "../features/application/applicationSlice";
+import courses from "../features/courses/coursesSlice";
+
 const store = configureStore({
   reducer: {
     application,
+    courses
   },
 });
 
